b04e89cdc322f5c747676cf420a611e4ec9a1197
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-04-14 18:39:19 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(obj => renderBooks(obj));
 }
 
 function renderBooks(books) {
@@ -12,6 +13,4 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener('DOMContentLoaded', fetchBooks);
